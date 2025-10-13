71da19af3765a88c5c155583185ed21c5838b7df
VERNAGUT-Titouan-2326112-A <titouan.vernagut@etu.univ-amu.fr>
2025-10-13 19:59:37 +0200

    add sound to recover coins

--- Assets/Script/RecoverACoin.cs
+++ Assets/Script/RecoverACoin.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
 
+    public AudioClip collectSound; // Le son à jouer
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.CompareTag("Player"))
         {
+            audioSource.PlayOneShot(collectSound);
             // ajouter points, sons, etc.
 
-
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
             Destroy(gameObject);
+
+
+
         }
     }
 }
