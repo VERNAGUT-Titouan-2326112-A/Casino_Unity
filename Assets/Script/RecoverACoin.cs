96b8136658364c28109c07a8fef5fcd8eef83007
VERNAGUT-Titouan-2326112-A <titouan.vernagut@etu.univ-amu.fr>
2025-10-14 20:26:11 +0200

    add UI CounterCoin and IncrementationCoin

--- Assets/Script/RecoverACoin.cs
+++ Assets/Script/RecoverACoin.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
-    public AudioClip collectSound; // Le son à jouer
-    private AudioSource audioSource;
+    public AudioClip collectSound;
+    private AudioSource audioSource; 
+    public TextMeshProUGUI counterText;  
+    private static int score = 500;             
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        counterText.text = "" +score;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,13 +19,11 @@ public class NewBehaviourScript : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             audioSource.PlayOneShot(collectSound);
-            // ajouter points, sons, etc.
-
+           score += 50;
+           counterText.text = "" + score;
             AudioSource.PlayClipAtPoint(collectSound, transform.position);
             Destroy(gameObject);
-
-
-
         }
     }
 }
+    
