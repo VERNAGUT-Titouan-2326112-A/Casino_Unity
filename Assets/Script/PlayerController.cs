42fe4dfc33640897ed257bed488eccd8fd93fe2b
bapti <baptiste.armani@gmail.com>
2025-11-02 21:00:00 +0100

    Ajout de la fonction de gestes pour le personnage, maintenant il a des annimations

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,20 +6,42 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 6f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
     private CharacterController controller;
     private Vector2 moveInput;
+    private Vector3 velocity;
+    private bool isGrounded = true;
+    private Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // ✅ On vérifie si le joueur est au sol
+        Vector3 groundCheckPos = transform.position + Vector3.up * 0.2f;
+
+        // ✅ Si le joueur touche le sol et tombe, on stoppe la vitesse vers le bas
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        // ✅ Déplacement horizontal
         Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
         controller.Move(move * speed * Time.deltaTime);
+
+        // ✅ Gravité
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        // ✅ Animation : grounded et MoveSpeed
+        animator.SetBool("Grounded", isGrounded);
+        animator.SetFloat("MoveSpeed", move.magnitude);
     }
 
     void OnMove(InputValue value)
@@ -27,4 +49,11 @@ public class PlayerController : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    // ✅ Visualisation de la sphère de détection au sol
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = isGrounded ? Color.green : Color.red;
+        Vector3 groundCheckPos = transform.position + Vector3.up * 0.5f;
+        Gizmos.DrawWireSphere(groundCheckPos, groundDistance);
+    }
 }
